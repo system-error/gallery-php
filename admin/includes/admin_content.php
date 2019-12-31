bc647d9cf4267da997cd8c2aee395dc6e709d06e
system-error <christos_dousmanopoulos@yahoo.gr>
2019-12-31 10:18:02 +0200

    changes

---
+++ admin/includes/admin_content.php
@@ -0,0 +1,34 @@
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">
+                Blank Page
+                <small>Subheading</small>
+            </h1>
+            <?php
+                $sql = "SELECT * FROM users WHERE id = 1";
+                $result = $database->query($sql);
+                $user =mysqli_fetch_assoc($result);
+//                $user = mysqli_fetch_array($result,MYSQLI_ASSOC);
+//                 print_r($user);
+            foreach ($user as $u){
+                echo $u;
+            }
+
+            ?>
+            <ol class="breadcrumb">
+                <li>
+                    <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
+                </li>
+                <li class="active">
+                    <i class="fa fa-file"></i> Blank Page
+                </li>
+            </ol>
+        </div>
+    </div>
+    <!-- /.row -->
+
+</div>
+<!-- /.container-fluid -->
